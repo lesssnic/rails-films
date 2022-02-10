b7c9917b025ad18bc21c74317ca54b0119327c0a
lesssnic <antoxxxa.ua@gmail.com>
2022-02-10 12:36:17 +0200

    change actor validation

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,5 @@
 class Actor < ApplicationRecord
   has_many :film_actors, dependent: :destroy
   has_many :films, through: :film_actors
-  validates :name, null: false
+  validates :name, presence: true
 end
